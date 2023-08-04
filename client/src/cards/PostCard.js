45c6b7759f56e8fb0b7a1760bf4ec40d2e5d3379
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-08-04 15:38:21 -0600

    show username of post and link color change

--- client/src/cards/PostCard.js
+++ client/src/cards/PostCard.js
@@ -1,10 +1,14 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
 
 function PostCard ({ post }) {
+    const { user } = useContext(UserContext);
 
     return (
         <div className="box">
-            <h3><Link to={`/posts/${post.id}`}>{post.title}</Link></h3>
+            <h3><Link className="title-link" to={`/posts/${post.id}`}>{post.title}</Link></h3>
+            <h5>{post.creator.username}</h5>
             <p>{post.content}</p>
         </div>
     )
