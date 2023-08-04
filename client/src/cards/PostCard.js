bd815321dca50c7416bc0aa2c1dd5f28e6383135
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-08-03 21:09:45 -0600

    deleting post

--- client/src/cards/PostCard.js
+++ client/src/cards/PostCard.js
@@ -6,8 +6,6 @@ function PostCard ({ post }) {
         <div className="box">
             <h3><Link to={`/posts/${post.id}`}>{post.title}</Link></h3>
             <p>{post.content}</p>
-            <button>Edit</button>
-            <button>Delete</button>
         </div>
     )
 }
--- client/src/components/PostDetail.js
+++ client/src/components/PostDetail.js
@@ -1,16 +1,28 @@
 import { useContext } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { ContentContext } from "../context/ContentContext";
 
 function PostDetail () {
-    const { contents } = useContext(ContentContext);
+    const { contents, deletePost } = useContext(ContentContext);
     const id = parseInt(useParams().id);
     const post = contents.find(post => post.id === id);
+    const navigate = useNavigate();
+
+    const onDeletePost = () => {
+        fetch(`/posts/${post.id}`, {
+            method: "DELETE",
+        })
+        .then(resp => resp.json())
+        .then(deletePost(post.id))
+        .then(navigate('/posts'))
+    }
 
     return (
         <div>
             <h1>{post.title}</h1>
             <p>{post.content}</p>
+            <button>Edit</button>
+            <button onClick={onDeletePost}>Delete</button>
         </div>
     )
 }
