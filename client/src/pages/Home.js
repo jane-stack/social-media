126f0f76f0a894f9afb917a8e9dcc775d4f45e09
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-08-15 13:01:47 -0600

    fixed the commented_posts in home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,7 +9,7 @@ function Home () {
         <div className="home">
             <div>
                 <h4>Your Posts</h4>
-                {user.user_posts.map(post => (
+                {user.user_posts?.map(post => (
                     <div className="box-2" key={post.id}>
                         <h5><Link className="title-link" to={`/posts/${post.id}`}>{post.title}</Link></h5>
                     </div>
@@ -17,16 +17,32 @@ function Home () {
             </div>
             {/* <div>
                 <h4>Commented</h4>
-                {user.commented_posts.map(post => (
+                {user.commented_posts?.map(post => (
                     <div className="box-2" key={post.id}>
                         <h5><Link className="title-link" to={`/posts/${post.id}`}>{post.title}</Link></h5>
                     </div>
                 ))}
+            </div> */}
+            <div>
+                <h4>Commented</h4>
+                {user.commented_posts?.reduce((uniqueTitles, post) => {
+                    if (!uniqueTitles.includes(post.title)) {
+                        uniqueTitles.push(post.title);
+                        return uniqueTitles;
+                    }
+                    return uniqueTitles;
+                }, []).map((title, index) => (
+                    <div className="box-2" key={index}>
+                        <h5>
+                            <Link className="title-link" to={`/posts/${index}`}>{title}</Link>
+                        </h5>
+                    </div>
+                ))}
             </div>
-            <br/><br/><br/> */}
+            <br/><br/><br/>
             <div>
                 <h4>Liked</h4>
-                {user.liked_posts.map(post => (
+                {user.liked_posts?.map(post => (
                     <div className="box-2" key={post.id}>
                         <h5><Link className="title-link" to={`/posts/${post.id}`}>{post.title}</Link></h5>
                     </div>
